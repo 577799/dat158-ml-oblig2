b02a8d20ddf18fed394247c5a63d5ea4829be7df
577799 <577799@users.noreply.github.com>
2020-11-11 14:17:03 +0100

    bruker kan laste opp en fil

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+#from werkzeug import secure_filename
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("upload.html")
+    return redirect("/upload")
+
+@app.route('/upload')
+def upload_file():
+   return render_template("upload.html")
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_files():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(f.filename)
+      return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = True)
