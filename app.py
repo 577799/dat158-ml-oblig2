f63f24cd8b225e2407f3370b9f998361295f3166
577799 <577799@users.noreply.github.com>
2020-11-12 09:21:27 +0100

    laget nendlasting til nedlastings mappe

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
+import os.path
 #from werkzeug import secure_filename
 
+UPLOAD_FOLDER = 'nedlastninger'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 @app.route('/')
 def index():
@@ -15,8 +19,14 @@ def upload_file():
 def upload_files():
    if request.method == 'POST':
       f = request.files['file']
-      f.save(f.filename)
+     # f.save(f.filename) # denne fungerer men lagrer i feil mappe
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename)) #fjern denne om nedlasting ikke fungerer
       return 'file uploaded successfully'
 		
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 if __name__ == '__main__':
    app.run(debug = True)
