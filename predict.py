c6fff4e832c2a1aae178af80df2b151f85bf72ac
577799 <577799@users.noreply.github.com>
2020-11-12 13:13:43 +0100

    predict

---
+++ predict.py
@@ -0,0 +1,11 @@
+import matplotlib as plt
+import matplotlib.image as mpimg
+import joblib
+
+model = joblib.load('imageRec.ipynb')
+
+def predict():
+    #data = mpimg.imread('/nedlastninger')
+    ##imgplot = 
+    # plt.imshow(img)
+   # pred = ml-oblig
