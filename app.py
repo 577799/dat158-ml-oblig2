c6ddb499b73f0826ab8d9252bb3f7ab518654280
577799 <577799@users.noreply.github.com>
2020-11-18 10:39:41 +0100

    update

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def uploaded_file(filename):
 @app.route('/predict', methods=['GET', 'POST'])
 def predicting():
   asdf = pred.prediction()
-  return render_template("upload.html", result=asdf, imagesrc="nedlastninger/image.jpg")
+  return render_template("upload.html", result=asdf, imagesrc="static/image.jpg")
 
 if __name__ == '__main__':
    app.run(debug = True)
